93a7b244f7d115185b01121a9d3b6d551c90fbeb
SebastianMarAlv <sebastianmarquez0@gmail.com>
2020-12-08 11:45:52 -0600

    Major changes to structure of proyect

---
+++ Records.cpp
@@ -0,0 +1,29 @@
+//
+// Sebastian Marquez Alvarez A01632483.
+//
+//
+// 08/12/2020.
+
+#include "Records.h"
+
+Records::Records() {
+    string fileName;
+    string buff;
+    stringstream ss;
+    ifstream recordsFile;
+    string ip;
+    cout << "Input file with the records (include extension): "<< endl;
+    cin >> fileName;
+    recordsFile.open(fileName, ios::in);
+    while (recordsFile.fail()) {
+        cout << "Unable to open \"" << fileName << "\", input file with the records (include extension): ";
+        cin >> fileName;
+        recordsFile.open(fileName, ios::in);
+    }
+    while (recordsFile.is_open() && !recordsFile.eof()) {
+        getline(recordsFile, buff);
+        Record newRecord(buff);
+        data.push_back(newRecord);
+    }
+}
+
---
+++ Records.h
@@ -0,0 +1,18 @@
+//
+// Created by liu_k on 08/12/2020.
+//
+
+#ifndef ACT5_2_RECORDS_H
+#define ACT5_2_RECORDS_H
+
+#include "Record.h"
+
+class Records {
+    private:
+        std::vector<Record> data;
+    public:
+        Records();
+};
+
+
+#endif //ACT5_2_RECORDS_H
